1c03d0084fd998e1e8694190f33097b18864626b
Camila Fimbres <camifimbres@gmail.com>
2025-08-28 07:51:45 -0700

    Agregando clase contacto

---
+++ Agenda/Contacto.cs
@@ -0,0 +1,9 @@
+﻿namespace Agenda
+{
+    internal class Contacto
+    {
+      public string Nombre { get; set; }
+      public string Apellido { get; set; }
+      public string Email { get; set; }
+    }
+}
