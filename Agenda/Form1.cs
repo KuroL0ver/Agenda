b36498ea979c5f517301caeacd3a6b9c14b95082
Camila Fimbres <camifimbres@gmail.com>
2025-09-03 08:02:35 -0700

    Cambios de continue

--- Agenda/Form1.cs
+++ Agenda/Form1.cs
@@ -13,12 +13,21 @@ namespace Agenda
             {
                 //Lee solo las líneas del encabezado 
                 //Es un arreglo de strings
-                string[] datos = File.ReadAllLines(@"Datos\Personas.csv");
-                //Muestra la primera línea del archivo (Nombre, Apellido, Email)
-                //Si cambias el [0] por [1] muestra la segunda línea (Que sería el primer contacto)
-                MessageBox.Show(datos[0]);
+                string[] renglones = File.ReadAllLines(@"Datos\Personas.csv");
+
+                bool esContacto = false;
+                foreach (string renglon in renglones)
+                {
+                    if (esContacto == false)
+                    {
+                        continue;
+                    }
+
+                    esContacto = true;
+                    MessageBox.Show(renglon);
+                }
             }
-            catch (Exception ex) 
+            catch (Exception ex)
             {
 
                 MessageBox.Show(ex.Message);
